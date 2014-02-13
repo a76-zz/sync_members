e4199ece6562b077cfe1fc12b01acd633a1f94f8
Andrei Silchankau <andrei.silchankau@gmail.com>
2014-02-13 17:46:24 +0300

    Add transformation logic

--- src/sync_members_handler.erl
+++ src/sync_members_handler.erl
@@ -12,7 +12,7 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {channel}).
+-record(state, {channel, map}).
 
 -include_lib("deps/amqp_client/include/amqp_client.hrl").
 
@@ -49,7 +49,10 @@ init([]) ->
 
     amqp_channel:call(Channel, #'basic.qos'{prefetch_count = 1}),
     amqp_channel:subscribe(Channel, #'basic.consume'{queue = <<"members_sync">>}, self()),
-    {ok, #state{channel=Channel}}.
+    DefaultMap = dict:from_list([{"member_id", <<"id_i">>},
+            {"first_name", <<"first_name_s">>},
+            {"last_name", <<"last_name_s">>}]),
+    {ok, #state{channel=Channel, map=DefaultMap}}.
 
 
 %% @spec handle_call(Request, From, State) -> 
@@ -89,6 +92,9 @@ handle_info({#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = Body}}, S
     after
         Dots*1000 -> ok
     end,
+    Content = binary_to_term(Body),
+    io:format(" received:~p~n", [Content]),
+    save_content(Content, State#state.map, []),
     io:format(" [x] Done~n"),
     amqp_channel:cast(State#state.channel, #'basic.ack'{delivery_tag = Tag}),
     {noreply, State}.
@@ -98,7 +104,7 @@ handle_info({#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = Body}}, S
 %% @spec terminate(Reason, State) -> void()
 %% @doc This function is called by a gen_server when it is about to
 %% terminate. It should be the opposite of Module:init/1 and do any necessary
-%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% cleaning up. When it returns, the gen_selrver terminates with Reason.
 %% The return value is ignored.
 terminate(_Reason, _State) ->
     ok.
@@ -107,5 +113,49 @@ terminate(_Reason, _State) ->
 %% @doc Convert process state when code is changed
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% Private functions
+save_content(Content, Map, KeyMap) ->
+    case Content of 
+        {selected, ColNames, Rows} ->
+            save_content(ColNames, Rows, Map, KeyMap)
+    end.
+
+save(Key, Value) -> 
+    io:format("Saved:~p:~p~n", [Key, Value]).
+
+get_key(Source, _KeyMap) ->
+    {"member_id", Id} = lists:keyfind("member_id", 1, Source), 
+    string:concat("http://localhost:8098/buckets/members/keys/", erlang:term_to_list(Id)).
+
+save_content(ColNames, [Row|OtherRows], Map, KeyMap) ->
+    Source = lists:zip(ColNames, erlang:tuple_to_list(Row)),
+    Value = transform(Source, Map),
+    Key = get_key(Source, KeyMap),
+    save(Key, Value),
+    save_content(ColNames, OtherRows, Map, KeyMap);
+
+save_content(_ColNames, [], _Map, _KeyMap) ->
+    ok.
+
+transform(Source, Map) ->
+    transform(Source, Map, []).
+
+
+transform([{Key, Value}|Source], Map, Result) ->
+    case dict:find(Key, Map) of 
+        {ok, TKey} ->
+            TValue = 
+            if 
+                is_list(Value) -> erlang:list_to_binary(Value);
+                true -> Value
+            end,
+            transform(Source, Map, [{TKey, TValue}|Result]);    
+        error ->
+            transform(Source, Map, Result)
+    end;
+
+transform([], _Map, Result) ->
+    Result.
 
 
--- src/t.erl
+++ src/t.erl
@@ -10,4 +10,39 @@ create() ->
 
 	% curl 'http://localhost:8098/solr/members/select?q=first_name_s:jain&wt=json'
 
+% Sample data:
+%{selected,["member_id","first_name","last_name",
+%           "time_stamp"],
+%          [{1,"andrei","silchankau",{{2014,1,29},{14,28,14}}}]}
+
+
+transform(ColNames, Row, Map) ->
+	Source = lists:zip(ColNames, erlang:tuple_to_list(Row)),
+	transform_row(Source, Map, []).
+
+
+transform_row([{Key, Value}|Source], Map, Result) ->
+	case dict:find(Key, Map) of 
+		{ok, TKey} ->
+			TValue = 
+			if 
+				is_list(Value) -> erlang:list_to_binary(Value);
+				true -> Value
+			end,
+			transform_row(Source, Map, [{TKey, TValue}|Result]); 	
+		error ->
+			transform_row(Source, Map, Result)
+	end;
+
+transform_row([], _Map, Result) ->
+	Result.
+
+transform_test() ->
+	Data = transform(["member_id","first_name","last_name","time_stamp"],
+		{1,"andrei","silchankau",{{2014,1,29},{14,28,14}}},
+		dict:from_list([{"member_id", <<"id_i">>},
+			{"first_name", <<"first_name_s">>},
+			{"last_name", <<"last_name_s">>}])),
+	jsx:encode(Data).
+
 
